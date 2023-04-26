0f8b6f6647ebc5d496a901f3345bb3a31ed9ebcf
Parashpoudel9627 <parash@ecosystems.com.np>
2023-04-26 20:34:53 +0545

    first commit

---
+++ app/src/main/java/com/dandm/Checkin.java
@@ -0,0 +1,14 @@
+package com.dandm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Checkin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkin);
+    }
+}
